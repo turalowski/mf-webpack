51a39c4a31dcf317ef8c6810efd2349371effcd0
Tural Hajiyev <thajiyev9@outlook.com>
2024-04-13 17:30:17 +0200

    Added faker module to test module sharing

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,3 +1,10 @@
-const productCount = Math.floor(Math.random() * 100);
+const { faker } = require('@faker-js/faker');
+
+// Generate a random number between 0 and 100 using faker
+const productCount = faker.datatype.number({ min: 0, max: 100 });
+
+// Create the cart text
 const cartText = `<div>You have ${productCount} products</div>`;
+
+// Set the cart text to the element with id 'dev-cart'
 document.querySelector('#dev-cart').innerHTML = cartText;
--- products/src/index.js
+++ products/src/index.js
@@ -1,7 +1,10 @@
+const { faker } = require('@faker-js/faker');
+
 let products = '';
 
 for (let i = 1; i < 6; i++) {
-  products += `<div>product ${i}</div>`;
+  const productName = faker.commerce.productName();
+  products += `<div>${productName}</div>`;
 }
 
 document.querySelector('#dev-products').innerHTML = products;
