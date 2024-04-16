e1c19208dfb77956a47b1e6b33ce0e4a048349d9
Tural Hajiyev <thajiyev9@outlook.com>
2024-04-17 00:58:12 +0200

    reduced dependency between modules

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,22 @@
-const { faker } = require('@faker-js/faker');
+import { faker } from '@faker-js/faker';
 
-let products = '';
+const mount = el => {
+  let products = '';
 
-for (let i = 1; i < 6; i++) {
-  const productName = faker.commerce.productName();
-  products += `<div>${productName}</div>`;
+  for (let i = 1; i < 6; i++) {
+    const productName = faker.commerce.productName();
+    products += `<div>${productName}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products');
+
+  if (el) {
+    mount(el);
+  }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+export { mount };
